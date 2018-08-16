5f95f2271e5c30e85fe598087148c259c7b69b5a
alessandro <alessandro.maggio98@gmail.com>
2018-08-16 20:19:45 +0200

    Implementazione classe Transactions

---
+++ Transactions.h
@@ -0,0 +1,20 @@
+//
+// Created by alessandro on 16/08/18.
+//
+
+#ifndef FINANCIALMANAGER_TRANSACTIONS_H
+#define FINANCIALMANAGER_TRANSACTIONS_H
+
+
+class Transactions {
+
+protected:
+
+
+public:
+
+
+};
+
+
+#endif //FINANCIALMANAGER_TRANSACTIONS_H
